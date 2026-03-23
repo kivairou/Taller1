34e7baee213a4f4723c200c268674aaf81577c84
Kevin Zamora Riquelme <kevin.zamora@alumnos.ucn.cl>
2026-03-23 17:20:41 -0300

    update main

--- Taller1/src/taller1/Main.java
+++ Taller1/src/taller1/Main.java
@@ -1,10 +1,48 @@
 package taller1;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scan;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		leerUsuarios();
+		leerRegistros();
+		desplegarMenu();
+		
 
+	}
+
+	private static void desplegarMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void leerRegistros() throws FileNotFoundException{
+		
+		File txtRegistros = new File("Registros.txt");
+		scan = new Scanner(txtRegistros);
+		
+		while(scan.hasNextLine()) {
+			
+			String linea = scan.nextLine();
+			String[] partes = linea.split(";");
+			
+			String nombre = partes[0];
+			String fecha = partes[1];
+			int horas = Integer.parseInt(partes[2]);
+			String actividad = partes[3];
+		}
+		
+	}
+
+	private static void leerUsuarios() throws FileNotFoundException{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
