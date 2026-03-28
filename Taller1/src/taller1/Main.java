4812ce0c97d4ccd038c5a1fb242b7ec3d2e35987
tomaszepeda2411 <tomas.zepeda01@alumnos.ucn.cl>
2026-03-28 19:56:56 -0300

    Update menus

--- Taller1/src/taller1/Main.java
+++ Taller1/src/taller1/Main.java
@@ -1,3 +1,9 @@
+/*
+ Kevin Matías Mauricio Zamora Riquelme | 21578521-1 | ICCI
+ Tomás Ignacio Zepeda Velasquez | 21789061-6 | ICCI
+
+
+*/
 package taller1;
 
 import java.util.Scanner;
@@ -7,6 +13,7 @@ import java.io.FileNotFoundException;
 public class Main {
 
 	private static Scanner scan;
+	
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
@@ -19,11 +26,52 @@ public class Main {
 
 	private static void desplegarMenu() {
 		// TODO Auto-generated method stub
+		scan = new Scanner(System.in);
+		int opcion = 0;
+		
+		do {
+		System.out.println();
 		System.out.println("1) Menu de Usuarios");
 		System.out.println("2) Menu de Analisis");
 		System.out.println("3) Salir");
+		System.out.print("Ingrese una opcion:");
+		
+		opcion = scan.nextInt();
+		scan.nextLine();
+		
+		switch(opcion) {
+		
+		case 1:
+			
+			System.out.print("Usuario: ");
+			String user = scan.nextLine();
+			System.out.print("Contraseña: ");
+			String contra = scan.nextLine();
+			break;
+		case 2:
+			int opcion1 = 0;
+			do {
+			System.out.println();
+			System.out.println("Bienvenido al menu de analisis!");
+			System.out.println("1) Actividad mas realizada");
+			System.out.println("2) Actividad mas realizada por cada usuario");
+			System.out.println("3) Usuario con mayor procastinacion");
+			System.out.println("4) Ver todas las actividades");
+			System.out.println("5) Salir");
+			System.out.print("Ingrese una opcion:");
+			
+			opcion1 = scan.nextInt();
+			scan.nextLine();
+			switch(opcion1) {
+			
+			}
+			break;
+			}while(opcion1 !=5);
+		}
+	}while(opcion!=3); 
+		
 	}
-
+	
 	private static void leerRegistros() throws FileNotFoundException{
 		
 		File txtRegistros = new File("Registros.txt");
